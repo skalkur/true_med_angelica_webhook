fe636f6e3912608ed4886280a8488a949c9256c1
Sharath Kalkur <sharathkalkur1993@gmail.com>
2017-11-25 12:46:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,21 @@ from flask import make_response
 # Flask app should start in global layout
 app = Flask(__name__)
 
+patient_info={}
+strain_score={
+              "Snoop Dogg":400,
+              "Eminem":240,
+              "NWA":350,
+              "Akon":750,
+              "Dr. Dre":425
+              }
 
+def get_comparables(strain_taken):
+    global strain_score
+    return [k for k,v in strain_score.items() if v>strain_score[strain_taken]]
+
+
+             
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
@@ -28,17 +42,21 @@ def webhook():
     return r
 
 def makeWebhookResult(req):
+    global patient_info
     if req.get("result").get("action") == "re_iterate_info-yes":
-
         result=req.get("result")
         duration=result.get('contexts')[0].get('parameters').get('duration').get('date-period.original')
+        patient_info['duration']=duration
         parameters=result.get('parameters')
         person=parameters.get('person')
+        patient_info['person']=person
         gender=parameters.get('gender')
+        patient_info['gender']=gender
         problems=parameters.get('problems')
+        patient_info['problems']=problems
         other_symptoms_list=[parameters.get('secondary_symptom'),parameters.get('tertiary_symptom'),parameters.get('psycological_symptoms')]
         other_symptoms_list=([k for k in other_symptoms_list if k!='null'])
-        
+        patient_info['other_symptoms_list']=other_symptoms_list
     
       #  cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
     
@@ -59,34 +77,75 @@ def makeWebhookResult(req):
             "speech": speech+" May I know what strains do you use for your condition?",
             "displayText": speech+" May I know what strains do you use for your condition?",
             #"data": {},
-             "contextOut": [
-                            {
-                             "name":"strain",
-                             "parameters":{
-                                           "strain":"Snoop Dogg"
-                                           },
-                                           "lifespan":5
-                                           }
-                                           ],
+#             "contextOut": [
+#                            {
+#                             "name":"strain",
+#                             "parameters":{
+#                                           "strain":"Snoop Dogg"
+#                                           },
+#                                           "lifespan":5
+#                                           }
+#                                           ],
             "source": "apiai-angelica"
         }
         
     elif req.get("result").get("action")=="re-iterate_info-no":
         result=req.get("result")
         duration=result.get('contexts')[0].get('parameters').get('duration').get('date-period.original')
+        patient_info['duration']=duration
         parameters=result.get('parameters')
         person=parameters.get('person')
+        patient_info['person']=person
         gender=parameters.get('gender')
+        patient_info['gender']=gender
         problems=parameters.get('problems')
+        patient_info['problems']=problems
         other_symptoms_list=[parameters.get('secondary_symptom'),parameters.get('tertiary_symptom'),parameters.get('psycological_symptoms')]
         other_symptoms_list=([k for k in other_symptoms_list if k!='null'])
+        patient_info['other_symptoms_list']=other_symptoms_list
+
         speech="Alright! Goodbye!"
         return {
                 "speech":speech,
                 "displayText":speech,
                 "source":"apiai-angelica"
-                }
+       }
 
+    elif req.get("result").get("action")=="get_strain_info":
+        result=req.get("result")
+        strain_taken=result.get('parameters').get('strain')
+        comparables=get_comparables(strain_taken)
+        patient_info["strain_taken"]=strain_taken
+        patient_info["comparables"]=comparables
+        if comparables!=[]:
+            if len(comparables)>1:
+                comparables[-1]='or '+comparables[-1]
+                speech="Based on our observations, the better comparables for {} are either of {}.".format(strain_taken, ", ".join(comparables))
+            else:
+                speech="Based on our observations, the only better comparable for {} is {}.".format(strain_taken, "".join(comparables))
+    
+        else:
+            speech="As per my knowledge, you seem to be taking the most effective strain for {}".format(patient_info['problems']) 
+        print("Response:")
+        print(speech)
+        return {
+            "speech": speech+" I hope I was able to help you out, %s" %(patient_info['person']),
+            "displayText": speech+" I hope I was able to help you out, %s" %(patient_info['person']),
+            #"data": {},
+#             "contextOut": [
+#                            {
+#                             "name":"strain",
+#                             "parameters":{
+#                                           "strain":"Snoop Dogg"
+#                                           },
+#                                           "lifespan":5
+#                                           }
+#                                           ],
+            "source": "apiai-angelica"
+        }    
+        
+        
+        
     else:
         return {}
         
